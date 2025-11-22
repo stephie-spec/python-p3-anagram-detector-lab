6921af6cf375eb036ea0e6327e1646ccde9854fa
stephie-spec <compwork254@gmail.com>
2025-11-22 08:53:37 +0300

    writes anagram class

--- lib/anagram.py
+++ lib/anagram.py
@@ -1,1 +1,8 @@
-# your code goes here!
+class Anagram:
+    def __init__(self, word):
+        self.word = word.lower()
+    
+    def match(self, possible_anagrams):
+        sorted_word = sorted(self.word)
+        return [candidate for candidate in possible_anagrams 
+                if candidate.lower() != self.word and sorted(candidate.lower()) == sorted_word]
